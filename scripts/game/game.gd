0047c8fdb34a979d1351083f27e48ffbf215fc5a
Ryan Ginstrom <ryan@corpy.co.jp>
2026-08-02 16:30:21 +0900

    fix: handle escape before leaving game scene

--- scripts/game/game.gd
+++ scripts/game/game.gd
@@ -3,5 +3,5 @@ extends Node2D
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("ui_cancel"):
-		GameManager.go_to_main_menu()
 		get_viewport().set_input_as_handled()
+		GameManager.go_to_main_menu()
